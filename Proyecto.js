9dcbc7632e6266e2f8e259aab387c40a370f45ff
3st3b4nR <esteban.roldanrm@gmail.com>
2024-12-29 10:24:04 -0500

    Agregando la función "agregarproducto""

--- Proyecto.js
+++ Proyecto.js
@@ -1,2 +1,12 @@
-let inventario={};
+const inventario = [];
+let siguienteId = 1;
 
+function agregarProducto(producto) {
+    if (inventario.some(p => p.nombre.toLowerCase() === producto.nombre.toLowerCase())) {
+        console.log(`El producto con nombre "${producto.nombre}" ya existe en el inventario.`);
+    } else {
+        producto.id = siguienteId++;
+        inventario.push(producto);
+        console.log(`Producto agregado: ${JSON.stringify(producto)}`);
+    }
+}
